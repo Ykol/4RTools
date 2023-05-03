fecba915145d3ed2a350f2efa5179914d59367ff
Ykol <ykol1989@gmail.com>
2023-05-03 12:24:44 +0300

    add fix

--- Utils/AppConfig.cs
+++ Utils/AppConfig.cs
@@ -5,13 +5,13 @@ namespace _4RTools.Utils
 {
     internal class AppConfig
     {
-        public static string Name = "4RTools";
+        public static string Name = "4RNods";
         public static string ProfileFolder = "Profile\\";
         public static string Website = "https://www.4rtools.com.br";
         public static string GithubLink = "https://github.com/4RTools/4Rtools";
         public static string DiscordLink = "https://discord.gg/AtZ2fJVtBz";
         public static string _4RClientsURL = "https://raw.githubusercontent.com/4RTools/4RTools/main/supported_servers.json";
         public static string _4RLatestVersionURL = "https://api.github.com/repos/4RTools/4RTools/releases/latest";
-        public static string Version = "v2.3.0";
+        public static string Version = "v2.4.0";
     }
 }
